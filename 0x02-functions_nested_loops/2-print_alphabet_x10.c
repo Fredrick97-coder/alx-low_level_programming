688b3ae5b7a4d272046cb95b6ce94d5553275098
Fredrick97-coder <fredrickotabil97@gmail.com>
2022-12-13 15:33:22 -0800

    2-print_alphabet_x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints alphabets
+ *
+ * Description - prints 10x of all alphabets
+ *
+ * Return: void
+ */
+
+void print_alphabet_x10(void)
+{
+	int iterator = 1;
+
+	while (iterator <= 10)
+	{
+		char letter = 'a';
+		while (letter <= 'z')
+		{
+			_putchar(letter);
+			letter++;
+		}
+		_putchar('\n');
+
+		iterator++;
+	}
+}

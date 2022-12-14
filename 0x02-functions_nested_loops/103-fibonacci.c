4a30509a8e56380ae2858a81395c1e34963f8566
Fredrick97-coder <fredrickotabil97@gmail.com>
2022-12-13 22:24:31 -0800

    103-fibonacci

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * description - prints the sum of all even Fibonacci numbers below 4,000,000
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	long int total_sum, sum, first, second;
+
+	total_sum = 0;
+	sum = 0;
+	first = 0;
+	second = 1;
+
+	while (sum < 4000000)
+	{
+		sum = first + second;
+		sum % 2 == 0 ? total_sum += sum : 0;
+	
+		first = second;
+		second = sum;
+	}
+
+	printf("%ld\n", total_sum);
+
+	return (0);
+}

cfa746bff216e1cc307016c9c1a8ebe12b60272e
Fredrick97-coder <fredrickotabil97@gmail.com>
2022-12-22 07:34:37 -0800

    strncat

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _strncat - concats two string using atmost
+ * n bytes from src
+ * @dest: destination string
+ * @src: source string
+ * @n: n integral byte(s) type
+ *
+ * Return: some char string
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	char *ptr = dest;
+
+	while (*ptr != '\0')
+	{
+		ptr++;
+	}
+	do {
+		*ptr++ = *src++;
+
+	} while (*src != '\0' && --n);
+
+	*ptr = '\0';
+	return (dest);
+}

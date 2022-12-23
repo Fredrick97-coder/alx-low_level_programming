39b82c318d89236ca6af65efba9460adad8de6eb
Fredrick97-coder <fredrickotabil97@gmail.com>
2022-12-22 19:17:41 -0800

    leet

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -8,19 +8,19 @@
 
 char *leet(char *s)
 {
-	char *r = s;
-	char a[] = { 'a', 'e', 'o', 't', 'l' };
-	char n[] = { 4, 3, 0, 7, 1 };
+	char *res = s;
+	char aeotl[] = { 'a', 'e', 'o', 't', 'l' };
+	char num[] = { 4, 3, 0, 7, 1 };
 	int i = 0;
 
-	while (*s)
+	while (*res)
 	{
 		for (i = 0; i < 5; i++)
 		{
-			if (*s == a[i] || *s == a[i] - 32)
-				*s = n[i] + '0';
+			if (*res == aeotl[i] || *res == aeotl[i] - 32)
+				*res = num[i] + '0';
 		}
-		s++;
+		res++;
 	}
-	return (r);
+	return (s);
 }

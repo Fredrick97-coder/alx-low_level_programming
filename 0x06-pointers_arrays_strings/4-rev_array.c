bac8ee8f943a6e384f30677049117a88c2b933a6
Fredrick97-coder <fredrickotabil97@gmail.com>
2022-12-22 17:29:51 -0800

    strcmp

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * reverse_array - reverses the content of an
+ * array of integers
+ * @a: arr of integers
+ * @n: length of the array
+ *
+ * Return: void
+ */
+
+void reverse_array(int *a, int n)
+{
+	int half = n / 2;
+	int i, left, right;
+
+	i = 0;
+	n--;
+
+	while (i < half)
+	{
+		left = a[n - i];
+		right = a[i];
+		a[i] = left;
+		a[n - i] = right;
+	}
+}

9d5fc275b0ed2cea07c98f8245219dce7219cb40
Fredrick97-coder <fredrickotabil97@gmail.com>
2023-01-03 23:52:54 -0800

    function that searches a string for any of a set of bytes

--- 0x07-pointers_arrays_strings/strspn.c
+++ 0x07-pointers_arrays_strings/strspn.c
@@ -10,29 +10,31 @@
 
 unsigned int _strspn(char *s, char *accept)
 {
+	unsigned int count = 0;
+	unsigned int prev = 0;
 	int i, j;
-	unsigned int count, prev;
 
 	i = 0;
 	j = 0;
-	count = 0;
-	prev = 0;
 
-	for (; *(s + i) != '\0'; i++)
+	while (*(s + i) != '\0')
 	{
 		j = 0;
 		prev = count;
-
-		for (; *(accept + j) != '\0'; i++)
+		while (*(accept + j) != '\0')
 		{
 			if (*(s + i) == *(accept + j))
 			{
 				count++;
 			}
+			j++;
 		}
+		if (prev == count)
+		{
+			break;
+		}
+		i++;
 	}
-	if (prev == count)
-		break;
 
 	return (count);
 }
